6b9eb321b2b5949219b34518e3364f07d1ad43db
katy <lozanokate5@gmail.com>
2021-03-30 17:33:43 -0600

    Initial Commit

---
+++ GitExample/ContentView.swift
@@ -0,0 +1,31 @@
+//
+//  ContentView.swift
+//  GitExample
+//
+//  Created by catalina lozano on 30/03/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var isPresented = false
+    
+    var body: some View {
+        NavigationView{
+            Button("Show Modal"){
+                self.isPresented = true
+                
+            }.sheet(isPresented: $isPresented){
+                Text("This is the modal screen")
+            }
+        }
+    
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
